9fa777c6817f75732bda32ce8a46dcb1b4ea79d7
samarth patel <samarthpatel981@gmail.com>
2024-09-11 20:39:04 +0530

    new file

---
+++ 2_node_export_inport/script.js
@@ -0,0 +1,8 @@
+import { generate } from "random-words";
+//in therer use this pakage import export so creat json file and type: module .
+
+console.log(generate());
+//output: 'army'
+
+console.log(generate(5));
+//output: ['army', 'beautiful', 'became', 'if', 'actually']
