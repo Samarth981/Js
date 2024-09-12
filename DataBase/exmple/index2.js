61915f3a62802b2dcc7ee3f8160b5ece0033df99
samarth patel <samarthpatel981@gmail.com>
2024-09-12 16:18:22 +0530

    new file

--- DataBase/exmple/index2.js
+++ DataBase/exmple/index2.js
@@ -3,8 +3,13 @@ const mysql = require("mysql2");
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended : true})); //data pase as a redabal formate
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "..", "views"));
+
 const connection = mysql.createConnection({
     host : "localhost",
     user : "root",
@@ -35,6 +40,7 @@ let getRandomUser = ()=>{
         }
     });
 
+    //show all user
     app.get("/users", (req,res) =>{
         let q = `SELECT * FROM user ORDER BY username ASC`;
         try{
@@ -47,6 +53,51 @@ let getRandomUser = ()=>{
         }
     });
 
+    //edit form open 
+    app.get("/users/:id/edit", (req,res) => {
+        let {id} = req.params;
+        let q = `SELECT * FROM user WHERE id = '${id}'`;
+        try{
+            connection.query(q,(err,result) => {
+                if(err) throw err;
+                let user = result[0];
+                res.render("edit.ejs", {user});
+            });
+        } catch(err){
+            res.render("some error found");
+        }
+    });
+
+    //update in DB 
+    app.patch("/users/:id", (req,res) => {
+        let {id} = req.params;
+        let {password : formpass, username : newusername} = req.body;
+        let q = `SELECT * FROM user WHERE id = '${id}'`;
+
+        try{
+            connection.query(q,(err,result) => { //comper query
+                if(err) throw err;
+                let user = result[0];
+                if(formpass != user.password){
+                    res.send("rong password");
+                }
+                else{
+                    //update query
+                    let q2 = `UPDATE user SET username= '${newusername}' WHERE id = '${id}'`;
+                    try{
+                        connection.query(q2,(err,result) => {
+                            if(err) throw err;
+                            res.redirect("/users");
+                        });
+                    } catch(err){
+                        res.send("some error found");
+                    }
+                }
+            });
+        } catch(err){
+            res.send("some error found");
+        }
+    });
 
 app.listen("8080", () => {
     console.log("server is working well");
