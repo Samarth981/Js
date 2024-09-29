f20cbff4d151427fd2a6ace179fa82a91e2c1996
samarth patel <samarthpatel981@gmail.com>
2024-09-29 17:30:10 +0530

    new file

--- MONGO/mogodb- Express/index2.js
+++ MONGO/mogodb- Express/index2.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./model/chat.js");
+const { create } = require("domain");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -16,6 +18,13 @@ async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
+let chat1 = new Chat({
+    from : "raj",
+    to : "samarth", 
+    msg : "what are you doing",
+    created_at : new Date(),
+});
+ 
 app.get("/", (req,res) => {
     res.send("root is working");
 });
