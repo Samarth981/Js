074ebcc40ece629bebc06a06ac814c0e00c93cca
samarth patel <samarthpatel981@gmail.com>
2024-11-26 09:08:28 +0530

    modified

--- MONGO/books.js
+++ MONGO/books.js
@@ -1,42 +1,43 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-main().then( () => {
-    console.log("serever is start");
-}).catch((err) => {
+main()
+  .then(() => {
+    console.log('serever is start');
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
 }
 
 const bookSchema = new mongoose.Schema({
-    title: 
-    {
-        type: String,
-        required : true,
-        maxLength : 20,
-    },
+  title: {
+    type: String,
+    required: true,
+    maxLength: 20,
+  },
 
-    author: {
-        type: String,
-    },
+  author: {
+    type: String,
+  },
 
-    price: {
-        type: Number,
-        min : [1, "price is so low please change it"],
-    },
-    descount : {
-        type : Number,
-        default : 0
-    },
-    category : {
-        type : String,
-        enum : ["fiction", "non-ficton"],
-    }
+  price: {
+    type: Number,
+    min: [1, 'price is so low please change it'],
+  },
+  descount: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: String,
+    enum: ['fiction', 'non-ficton'],
+  },
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model('Book', bookSchema);
 
 // const book1 = new Book({
 //     title : "Student of the year",
@@ -45,14 +46,19 @@ const Book = mongoose.model("Book", bookSchema);
 //     category : "fiction",
 // });
 // book1.save()
-//     .then((res) => console.log(res))  
+//     .then((res) => console.log(res))
 //     .catch((err) => console.log(err.errors.price.properties.message));
-
 
 //if update value then use [option.runvalidator]
 //{includeResultMetadata : true} use for give updateted value
 
-Book.findByIdAndUpdate("66f8d8df2257a022c951f47b", 
-    {price : 125}, {author : "rahul"}, {title : "min cost"},{runValidators: true}, {includeResultMetadata : true})
-    .then((res) => console.log(res))  
-    .catch((err) => console.log(err));
+Book.findByIdAndUpdate(
+  '66f8d8df2257a022c951f47b',
+  { price: 125 },
+  { author: 'rahul' },
+  { title: 'min cost' },
+  { runValidators: true },
+  { includeResultMetadata: true },
+)
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
